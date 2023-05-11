a3c928072b96c1078f24219a29a48c223c996f41
Sadula Rushidhar <115465446+S-Rushidhar-1-1999@users.noreply.github.com>
2023-05-11 19:41:07 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -860,7 +860,7 @@ if ospath.exists('categories.txt'):
             else:
                 CATEGORY_INDEX.append('')
 
-if BASE_URL:
+if True:
     Popen(f"gunicorn web.wserver:app --bind 0.0.0.0:{SERVER_PORT}", shell=True)
 
 srun(["qbittorrent-nox", "-d", "--profile=."])
